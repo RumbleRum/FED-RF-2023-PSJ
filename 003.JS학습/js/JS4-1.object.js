b9c1935f43e83d56cd8849e055560fc8c1df6706
RumbleRum <dnjswls0339@naver.com>
2023-08-28 17:16:41 +0900

    6666

---
+++ 003.JS학습/js/JS4-1.object.js
@@ -0,0 +1,174 @@
+// 영화 초이스 객체
+
+// 자신만의 오브젝트!
+const zoe_obj = {};
+// 오브젝트 형만 만들고 객체내용은 아래에서!
+// 1. 영화제목
+zoe_obj.title = "이터널 션샤인";
+// 2. 감독
+zoe_obj.director = "미셸 공드리";
+// 3. 배우
+zoe_obj.actor = "짐캐리, 케이트 윈슬렛";
+// 4. 장르
+zoe_obj.genre = "로맨스";
+// 5. 관람가
+zoe_obj.ratings = "12세";
+
+// 6. 예고편
+zoe_obj.trailer = function () {
+    console.log("예고편:", "07-QBnEkgXU");
+    // 플레이어함수 호출!
+    playMovie("07-QBnEkgXU");
+}; ////// trailer 함수 ////////
+
+       // 자신만의 오브젝트!
+       const nick_obj = {};
+       // 오브젝트 형만 만들고 객체내용은 아래에서!
+
+       // 1. 영화제목
+       nick_obj.title = '공조2:인터내셔날';
+
+       // 2. 감독
+       nick_obj.director = '이석훈';
+
+       // 3. 배우
+       nick_obj.actor = '현빈, 유해진, 윤아, 다니엘 헨리, 진선규';
+
+       // 4. 장르
+       nick_obj.genre = '액션,코미디';
+
+       // 5. 관람가
+       nick_obj.ratings = '15세';
+
+       // 6. 예고편
+       nick_obj.trailer = function(){
+           console.log('예고편:','fzUKUfHeIYA');
+           // 플레이어함수 호출!
+           playMovie('fzUKUfHeIYA');
+       } ///////////// trailer 함수 ///////////////
+
+        // 자신만의 오브젝트!
+        const shin_Obj = {};
+        // 오브젝트 형만 만들고 할당은 아래에서
+        // 1. 영화제목
+        shin_Obj.title = '말아톤';
+        // 2. 감독
+        shin_Obj.director = '정윤철';
+        // 3. 배우
+        shin_Obj.actor = '조승우,김미숙';
+        // 4. 장르
+        shin_Obj.genre = '드라마';
+        // 5. 관람가
+        shin_Obj.ratings = '전체 관람가';
+        // 6. 예고편
+        shin_Obj.trailer = function(){
+            console.log('예고편','mGgYQOiUq4s');
+            // 플레이어함수 호출
+            playMovie('mGgYQOiUq4s');
+
+        }/////////////trailer함수////////////////
+
+         // 자신만의 Object
+         const csj_obj = {};
+
+         // 오브젝트 형만 만들고 객체내용은 아래에서!
+         // 1. 영화제목
+         csj_obj.title = '가디언즈오브갤럭시vol.3';
+         // 2. 감독
+         csj_obj.director = '제임스건';
+         // 3. 배우
+         csj_obj.actor = '크리스프랫,조샐다나';
+         // 4. 영화장르
+         csj_obj.genre = 'SF코미디어드벤처';
+         // 5. 관람가
+         csj_obj.ratings = '12세';
+         // 6. 예고편
+         csj_obj.trailer = function(){
+         console.log('예고편:','XyHr-s3MfCQ');    
+
+             // 플레이어함수 호출!!
+             playMovie('XyHr-s3MfCQ');
+
+         }  ////////////// trailer 함수 ////////////////////
+
+         //자신만의 오브젝트!
+         const hg_obj = {};
+         // 오브젝트 형만 만들고 객체 내용은 아래에서 !
+
+         // 1. 영화제목
+         hg_obj.title = '엘리멘탈';
+
+         // 2. 감독
+         hg_obj.director = "피터 손";
+
+         // 3. 배우
+         hg_obj.actor = "레아 루이스";
+
+         // 4. 장르
+         hg_obj.genre = "애니메이션";
+
+         // 5. 관람가
+         hg_obj.ratings = "전체관람가";
+
+         // 6. 예고편
+         hg_obj.trailer = 
+         function(){
+             console.log('예고편:',"BOqFRHCrN-k");
+             //플레이어 함수 호출!
+             playMovie("BOqFRHCrN-k");
+             
+         }//////////////////trailer함수/////////////////////
+
+         
+            // 자신만의 오브젝트 -> 변경가능하게 let으로!!
+            const seo_obj1 = {};
+            // 오브젝트 형만 만들고 객체 내용은 아래에서!!
+            // 1. 영화제목
+            seo_obj1.title = '너의 결혼식';
+
+            // 2. 영화감독
+            seo_obj1.director = '이석근';
+
+            // 3. 영화배우
+            seo_obj1.actor = '박보영,김영광';
+
+            // 4. 영화장르
+            seo_obj1.genre = '멜로';
+
+            // 5. 관람가
+            seo_obj1.ratings = '12세 이상';
+
+            // 6. 예고편
+            seo_obj1.trailer = function(){
+                console.log('예고편:','HuQ6YFDYY0s');
+                // 플레이어 함수 호출
+                playMovie('HuQ6YFDYY0s');
+            
+            } /////////트레일러 함수//////////            
+
+
+            // 자신만의 오브젝트
+            const kimyoonmin_obj = {
+            };
+
+            // 오브젝트 형만 만들고 객체내용은 아래에서!
+            // 1. 영화제목
+            kimyoonmin_obj.title = '인셉션';
+
+            // 2. 감독
+            kimyoonmin_obj.director = '크리스토퍼 놀란';
+
+            // 3. 배우
+            kimyoonmin_obj.actor = '레오나르도 디카프리오, 조셉 고든 레빗';
+
+            // 4. 장르
+            kimyoonmin_obj.genre = '액션';
+
+            // 5. 관람가
+            kimyoonmin_obj.ratings = '12';
+
+            // 6. 예고편
+            kimyoonmin_obj.trailer = function(){
+                    console.log('예고편: ', '1FdB6LPSMkI');
+                    playMovie('1FdB6LPSMkI');
+            }; /////////////trailer 함수 ////////////////////
