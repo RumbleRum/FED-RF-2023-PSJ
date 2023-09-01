ae290aeb44968fcacb276bb8100a434c246c310f
RumbleRum <dnjswls0339@naver.com>
2023-09-01 11:38:01 +0900

    2

--- 002.CSS학습/05.3DAni/js/cube.js
+++ 002.CSS학습/05.3DAni/js/cube.js
@@ -10,3 +10,40 @@
     버튼으로 변경되어 있음!
 
 *************************************/
+// window 로드 이벤트 호출 //
+window.addEventListener('DOMContentLoaded',loadFn);
+
+// DOM 선택함수  /////
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+////////// 로딩함수 //////////////////
+function loadFn(){
+    //함수호출 확인
+    console.log('로딩완료!');
+
+    // 1. 대상선정: .btngo
+    // 1-1. 이벤트대상 : .btngo
+    const btngo = qs('.btngo');
+    // 1-2. 변경대상 : .cube
+    const cube = qs('.cube');
+    console.log('대상:',btngo,cube);
+
+    // 2. 이벤트 함수 설정하기
+    btngo.onclick = e => { // e- 이벤트 전달변수
+        // 큐브의 호출
+        console.log('R U N');
+        // 1.대상 .cube ->큐브변수
+
+        // 2.변경내용  : 클래스 on없으면 넣고 있으면 빼기
+        // classList.toggle(클래스명)
+        cube.classList.toggle('on');
+
+        // 3. 텍스트 읽어오기(버튼 글자 변경 위해)
+        console.log("화살표함수에서 this:",this,
+        "화살표함수에서 e.target",e.target);
+    }; /////// click 이벤트 함수 //////////
+
+
+
+} /////////loadFn함수//////////////////////////////
