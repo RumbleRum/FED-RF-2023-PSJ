2c5f87602f05c3e334295c853cc0dfe6d880c525
RumbleRum <dnjswls0339@naver.com>
2023-09-03 18:07:33 +0900

    2

---
+++ 700.쉐도잉 프로젝트/01.DKB_Pj/03.구현소스/js/main.js
@@ -0,0 +1,14 @@
+// 도깨비 PJ 메인 JS - main JS
+
+// 로딩구역 호출
+window.addEventListener('DOMContentLoaded',loadFn);
+
+// 로딩구역 함수 구역/////////////////////////////
+function loadFn(){
+    // 로딩 호출 확인
+    console.log('로딩완료!!');
+
+    // 뷰드러운 스크롤 적용
+    startSS();
+
+} //////loadFn //////////////////////////////////
