fdfd46012ae9c4df6972ba69a86ee6ba35343b87
RumbleRum <dnjswls0339@naver.com>
2023-09-03 18:26:34 +0900

    4444

--- 700.쉐도잉 프로젝트/01.DKB_Pj/03.구현소스/js/main.js
+++ 700.쉐도잉 프로젝트/01.DKB_Pj/03.구현소스/js/main.js
@@ -11,4 +11,23 @@ function loadFn(){
     // 뷰드러운 스크롤 적용
     startSS();
 
+    // 마우스휠 이벤트 막기가 작동되어
+    // 캐릭터 설명 작은박스 스크롤 작용이 안되므로
+    // 따라서 이벤트 버블링을 막아줘야함!!
+    // event.stopPropagation()
+    // 이벤트 객체의 이벤트 버블링 막아주는 매서드임!!
+
+    // 대상선정 :.desc-box
+    let desc_box = document.querySelectorAll('.desc-box');
+
+    // 모든 캐릭터 설명박스는 이벤트 버블링 막기!!!!
+    // -> 여기서 마우스 휠됨!!
+    desc_box.forEach(ele=>{  
+        ele.onwheel = e => e.stopPropagation();
+    });
+
+    // ele - 요소자신 !
+
+
+
 } //////loadFn //////////////////////////////////
