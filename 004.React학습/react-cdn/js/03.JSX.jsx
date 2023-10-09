65cc241178ecbec2a25225a1639ecb0ea2fb3b09
RumbleRum <dnjswls0339@naver.com>
2023-10-10 00:26:47 +0900

    33

--- 004.React학습/react-cdn/js/03.JSX.jsx
+++ 004.React학습/react-cdn/js/03.JSX.jsx
@@ -7,24 +7,22 @@
     - appendChild() 메서드 없이 DOM에 요소넣기가 가능함!
 *****************************************************/
 
-
 // 1. JSX를 사용한 것과 JSX를 사용하지 않은 것을 비교 ///////
 // (1) JSX를 사용한 예 ///////////
 // (1-1) 넣을 요소 만들기
 const myEle1 = <h1>나는 JSX를 사용하고 있어!</h1>;
 // (1-2) 리액트 루트 선택하기 : createRoot() 매서드 사용!
-const root1 = ReactDOM.createRoot(document.querySelectorAll('#root>div')[0]);
+const root1 = ReactDOM.createRoot(document.querySelectorAll("#root>div")[0]);
 // (1-3) 요소 출력하기 :  생성된 루트에 render() 메서드를 붙여서 사용
 root1.render(myEle1);
 
 // (2) JSX를 사용하지 않는 방법 //////////////
 // -> 넣을 요소를 createElement() 메서드로 생성해야함!(JSX를 쓰지않음)
-const myEle2 = React.createElement("h1",{},"나는 JSX를 쓰지않아");
+const myEle2 = React.createElement("h1", {}, "나는 JSX를 쓰지않아");
 // createElement(요소명,{JS코드작성},요소내용)
 
 // 두번째에 출력하기 ///////
-ReactDOM.render(myEle2,document.querySelectorAll('#root>div')[1]);
-
+ReactDOM.render(myEle2, document.querySelectorAll("#root>div")[1]);
 
 /***************************************************** 
     [ 출력방식 정리! ]
@@ -53,15 +51,14 @@ ReactDOM.render(myEle2,document.querySelectorAll('#root>div')[1]);
 *****************************************************/
 
 // 표현식에 쓸 변수
-let num1 = 1000, num2 = 7;
-
+let num1 = 1000,
+    num2 = 7;
 
 // 3. JSX 표현식 사용하기 ////////////////////////////////////////
-const myEle3 = <h1>리액트는 {num1*num2} 번 사용해도 좋다!</h1>
+const myEle3 = <h1>리액트는 {num1 * num2} 번 사용해도 좋다!</h1>;
 
 // 세번째에 출력하기 ///////
-ReactDOM.render(myEle3,document.querySelectorAll('#root>div')[2]);
-
+ReactDOM.render(myEle3, document.querySelectorAll("#root>div")[2]);
 
 // 4. 다중요소 html 블록 삽입하기 ////////////////////////
 const myEle4 = (
@@ -76,7 +73,7 @@ const myEle4 = (
 );
 
 // 네번째에 출력하기 ///////
-ReactDOM.render(myEle4,document.querySelectorAll('#root>div')[3]);
+ReactDOM.render(myEle4, document.querySelectorAll("#root>div")[3]);
 
 /***************************************************** 
     [ JSX 태그요소 작성시 여러줄일 경우 ]
@@ -101,25 +98,162 @@ ReactDOM.render(myEle4,document.querySelectorAll('#root>div')[3]);
 
 // 기본 데이터(배열)
 const mydata = [
-    {idx:1,name:"김수현",movie:"언제왔니"},
-    {idx:2,name:"장우혁",movie:"형님,형~~~님"},
-    {idx:3,name:"김혜수",movie:"내가쎈언니야"},
+    { idx: 1, name: "김수현", movie: "언제왔니" },
+    { idx: 2, name: "장우혁", movie: "형님,형~~~님" },
+    { idx: 3, name: "김혜수", movie: "내가쎈언니야" },
 ];
 
 // 출력 형식 : <li>배우명 : 영화명</li>
 // 배열변슈.map() 을 사용 -> 여기서도 맵.조인 인가?
+// -> JS map()과는 다른 별도의 출력처리가 이루어짐!
+// 결론 : map() 만 사용하여 바로 출력가능!
 
 const myEle5 = (
     <React.Fragment>
         <h2>[ 배우 리스트 ]</h2>
         <ul>
-            {mydata.map(v=><li>{v.name}:{v.movie}</li>)}
+            {mydata.map((v) => (
+                <li>
+                    {v.name}:{v.movie}:{v.idx == 3 ? "예뻐💖" : "멋쪄!🔞"}
+                </li>
+            ))}
         </ul>
     </React.Fragment>
 );
 
-// 네번째에 출력하기 ///////
-ReactDOM.render(myEle5,document.querySelectorAll('#root>div')[4]);
+// 다섯번째에 출력하기 ///////
+ReactDOM.render(myEle5, document.querySelectorAll("#root>div")[4]);
+
+/***************************************************** 
+    [ JSX는 홀로태그라도 끝에 닫기를 해줘야한다! ]
+    예) <br> -> <br />
+        <input type="text"> -> <input type="text" />
+*****************************************************/
+
+
+// 6. 홀로태그 출력해 보기 /////////////////////////////
+const myEle6 = <input type="text" value = "홀로태그는 스스로 닫아라!"/>;
+
+// 여섯번째에 출력하기 ///////
+ReactDOM.render(myEle6, document.querySelectorAll("#root>div")[5]);
+
+
+/***************************************************** 
+ [ JSX에서 속성 클래스는 className 으로 표기한다! ]
+ <태그 class="클래스명">
+ class는 JS에서 키워드이므로 사용못함! 대신...
+ <태그 className="클래스명">
+ *****************************************************/
+
+// 7. 속성으로 클래스 셋팅하여 사용하기 //////////////////////
+const myEle7 = <h1 className="myclass">className 속성으로 클래스를 셋팅한다!</h1>;
+
+
+// 일곱번째에 출력하기 ///////
+ReactDOM.render(myEle7, document.querySelectorAll("#root>div")[6]);
+
+
+/***************************************************** 
+ [ JSX에서 조건문 사용하기 - if문 ]
+ 리액트는 if명령문을 지원하지만
+ JSX내부에서는 지원하지 않는다!!!
+ 
+ JSX에서 조건문을 사용하려면?
+ JSX 외부에서 if문을 사용하거나
+ 아니면 내부에서 삼항연산자를 사용할 수 있다!
+ *****************************************************/
+
+// 8. JSX 외부에서 if문 사용하여 출력하기 /////////////
+const x = 100;
+let txt = '이 돈으로는 충분히 살 수있어 ㅋ'
+if(x<10000){
+    txt = '돈이 부족해서 살수 없어!시발'
+}
+
+// 출력변수
+const myEle8 = (
+    <div>
+        <h1>현재 내가 가진돈은 {x} 만언! </h1>
+        <h1>{txt}</h1>
+    </div>
+);
+
+
+// 여덞번째에 출력하기 ///////
+ReactDOM.render(myEle8, document.querySelectorAll("#root>div")[7]);
+
+
+// 9. JSX의 표현식에 삼항연산자 사용하여 출력하기    ///////////////
+let time = 7;
+
+const myEle9 = (
+    <React.Fragment>
+    <h1>지금 메시지? {time} 씨야!</h1>
+    <h1>{time>9?"지금집에 들어와":"더 놀다와"}</h1>
+</React.Fragment>
+
+);
+
+
+// 9번째에 출력하기 ///////
+ReactDOM.render(myEle9, document.querySelectorAll("#root>div")[8]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
