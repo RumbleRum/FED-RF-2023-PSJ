d6bd292513d755f1a6a4c9d3510739690032c510
RumbleRum <dnjswls0339@naver.com>
2023-10-19 18:08:59 +0900

    123321

--- 004.React학습/react-ex/js/data.js
+++ 004.React학습/react-ex/js/data.js
@@ -1,26 +1,135 @@
 export default [
     {
         "idx":1,
-        "gname":"나는야옷이야1",
-        "category":"men",
-        "gprice":"230000"
+        "gname":"반스어얼리",
+        "category":"kids",
+        "gprice":"55000"
     },
+
     {
         "idx":2,
-        "gname":"나는야옷이야2",
-        "category":"men",
-        "gprice":"330000"
+        "gname":"반스어얼리",
+        "category":"woman",
+        "gprice":"95000"
     },
+
     {
         "idx":3,
-        "gname":"나는야옷이야3",
-        "category":"women",
-        "gprice":"430000"
+        "gname":"반스어얼리",
+        "category":"kids",
+        "gprice":"85000"
     },
+
     {
         "idx":4,
-        "gname":"나는야옷이야4",
-        "category":"women",
-        "gprice":"530000"
-    }
+        "gname":"반스뉴진스",
+        "category":"woman",
+        "gprice":"75000"
+    },
+
+    {
+        "idx":5,
+        "gname":"반스어얼리",
+        "category":"men",
+        "gprice":"95000"
+    },
+
+    {
+        "idx":6,
+        "gname":"반스뉴진스",
+        "category":"kids",
+        "gprice":"45000"
+    },
+
+    {
+        "idx":7,
+        "gname":"반스토끼",
+        "category":"men",
+        "gprice":"95000"
+    },
+
+    {
+        "idx":8,
+        "gname":"반스뉴진스",
+        "category":"kids",
+        "gprice":"55000"
+    },
+
+    {
+        "idx":9,
+        "gname":"반스캐쥬얼",
+        "category":"woman",
+        "gprice":"45000"
+    },
+
+    {
+        "idx":10,
+        "gname":"반스토끼",
+        "category":"men",
+        "gprice":"45000"
+    },
+
+    {
+        "idx":11,
+        "gname":"반스토끼",
+        "category":"woman",
+        "gprice":"55000"
+    },
+
+    {
+        "idx":12,
+        "gname":"반스토끼",
+        "category":"kids",
+        "gprice":"85000"
+    },
+
+    {
+        "idx":13,
+        "gname":"반스토끼",
+        "category":"kids",
+        "gprice":"95000"
+    },
+
+    {
+        "idx":14,
+        "gname":"반스어얼리",
+        "category":"men",
+        "gprice":"45000"
+    },
+
+    {
+        "idx":15,
+        "gname":"반스토끼",
+        "category":"woman",
+        "gprice":"85000"
+    },
+
+    {
+        "idx":16,
+        "gname":"반스토끼",
+        "category":"kids",
+        "gprice":"95000"
+    },
+
+    {
+        "idx":17,
+        "gname":"반스캐쥬얼",
+        "category":"woman",
+        "gprice":"45000"
+    },
+
+    {
+        "idx":18,
+        "gname":"반스토끼",
+        "category":"men",
+        "gprice":"75000"
+    },
+
+    {
+        "idx":19,
+        "gname":"반스어얼리",
+        "category":"woman",
+        "gprice":"85000"
+    },
+
 ]
