3bd7d70fb232191591094158a145c62cd72c18ad
RumbleRum <dnjswls0339@naver.com>
2023-10-20 16:22:31 +0900

    545

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -1,25 +1,93 @@
 // 01.공유신발 JSX
 import myData from './data.js';
+import myData2 from './data2.js';
 
+// 두개의 데이터를 배열로 구성
+const twoData = [myData,myData2];
+
+// console.log(twoData);
+
+// console.log('여성의류',myData2);
 
 // 메인 컴포넌트 /////////////////
 // 메인의 의미는? 다른 구성요소 컴포넌트들을 모아
 // 최종적으로 랜더링하는 구성 컴포넌트를 말한다!
 function MainComponent(){
 
+    // 상태관리 메서드를 사용하여 후크변수를 설정하자!
+    // const [변수명,set변수명] = React.useState(초기값)    
+    // dataNum은 데이터를 구분하는 번호저장 후크변수다!
+    // 데이터 구분값으로 배열 순번을 생각하여 처음에 로딩될 
+    // 데이터가 0번째 즉 첫번째 배열순번 데이터를 불러올 순번값으로 셋팅한다!
+    const [dataNum,setDataNum] = React.useState(0);
+    
+    console.log('초기값',dataNum);
+
+    // 가상돔에서 실제 돔에 반영 후 DOM에 구현할 JS코드는 어디에 넣어야되나?
+    // ->> useEffect()
+    // ->> useLayoutEffect()
+
+    console.log('컴포넌트 그냥규욕',
+        document.querySelector('.img-box'));
+        
+        // -> 페이지 로딩후 >> 매법 업데이트 시에도 실행함!!
+        React.useEffect(()=>{
+            console.log('useEffect순수');
+            // console.log('useEffect순수구욕 JS',
+            //     document.querySelector('.img-box'));
+
+            // console.log('useEffect순수구욕 제이쿼리',
+            //     $('.img-box'));
+
+                
+            }); ////////////////////////////////////////////
+
+
+    // 빈 배열옵션 useEffect
+    // -> 페이지 로딩후 >> 단 한번만 실행함 
+    React.useEffect(()=>{
+        console.log('useEffect 빈배열 JS');             
+    },[]); ////////////////////////////////////////////
+            
+    // 랜더링 후 화면 출력전 상태
+    React.useLayoutEffect(()=>{
+        console.log('useLayoutEffect구욕 JS')
+    });
+
+
+
+
+    // 데이터 변경 호출 함수 /////////////////
+    const chgData = () => {
+        console.log('바꿔~!');
+        // 데이터 키 후크변수를 업데이트 함
+        setDataNum(dataNum?0:1);
+        // 업데이트 값 호출
+        console.log(dataNum);  
+    }; ///// chgData ////////////
+
+    // 최종 리턴 코드 ///////////////////////////
+    // 함수 , 변수 , 구현소스는 모두 return 위쪽에 코딩!
     return(
         <React.Fragment>
             {/* 1. 타이틀 */}
-            <h1 className="tit">공유가 신고 다닌다는!</h1>
+            <h1 className="tit">{dataNum?'효진이 입고':'공유가 신고'} 다닌다는!</h1>
             {/* 2. 내용박스 */}
             <section>
-                <h2>공유는 오늘도 멋찝니다!</h2>
+                <h2>{dataNum?"어머나어마나이러지마":"공유는 오늘도 멋찝니다!"}</h2>
                 {/* 이미지 */}
-                <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
+                <div className="img-box">
+                    <img src={dataNum?"./images/vans/hyo.jpg":"./images/vans/gongyoo.jpg"} alt="멋진공유" />
+                </div>
             </section>
-            {/* 3. 상품리스트박스 */}
+            {/* 3. 데이터 변경 버튼 */}
+            {/* <button onClick = {()=>chgData();ccc();bbb();} className = "btn-gong"> */}
+            <button onClick = {chgData} className = "btn-gong">
+                {dataNum?"공유초이스 바로가기":"효진초이스 바로가기"}
+            </button>
+            {/* 4. 상품리스트박스 */}
             <div className="gwrap">
-                <GoodsCode />
+                <GoodsCode idx={dataNum} />
             </div>
         </React.Fragment>
     );
@@ -27,15 +95,23 @@ function MainComponent(){
 }; /////////// MainComponent //////////////////
 
 
-console.log(myData);
+// console.log(myData);
 
-
+// 서브 컴포넌트(자식컴포넌트 -> 부모컴포넌트로 부터 데이터를
+// props 속성을 통하여 전달 받는다!)
 // 상품 html 코드 구성 컴포넌트 /////////////////////
-function GoodsCode(){
-    return(myData.map((v)=>
+function GoodsCode(props){ // idx - 데이터 배열순번
+    console.log(props.idx);
+    // 선택 데이터
+    const selData = twoData[props.idx];
+    return selData.map((v)=> (
         <ol class="glist">
             <li>
-                <img src={"./images/vans/vans_" + v.idx + ".jpg"} alt="신발" />
+                <img src={
+                    props.idx?
+                    "./images/gallery/" + v.idx + ".jpg":
+                    "./images/vans/vans_" + v.idx + ".jpg"
+                } alt={props.idx?"드레스":"신발"} />
             </li>
             <li>{v.gname}</li>
             <li>가격: {v.gprice}원</li>
--- 004.React학습/react-ex/js/make_data.js
+++ 004.React학습/react-ex/js/make_data.js
@@ -1,8 +1,16 @@
 // 데이터 유형에 맞게 만들기
 
-let category = ["men","woman","kids"]
-let gname = ["반스캐쥬얼","반스어얼리","반스뉴진스","반스토끼",]
-let gprice = ["55000","45000","75000","85000","95000",];
+// [vans 신발 데이터 생성]
+// let category = ["men","woman","kids"]
+// let gname = ["반스캐쥬얼","반스어얼리","반스뉴진스","반스토끼",]
+// let gprice = ["55000","45000","75000","85000","95000",];
+
+// [여성의류 데이터 생성]
+let category = ["modern","casual"];
+let gname = ["워닝드레스","워스비버든","코코샤스넬라","포코로와드"];
+let gprice = ["123000","224000","253000","340000","520000"];
+
+
 // 호출시 : rdm[아이템개수]
 let rdm = (x)=> Math.floor(Math.random()*x)
 
@@ -13,7 +21,7 @@ bb += `
     {
         "idx":${x},
         "gname":"${gname[rdm(4)]}",
-        "category":"${category[rdm(3)]}",
+        "category":"${category[rdm(2)]}",
         "gprice":"${gprice[rdm(5)]}"
     },
 `;
