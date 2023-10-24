af9934a1206503d0360ef432d9600944509af848
RumbleRum <dnjswls0339@naver.com>
2023-10-24 20:22:39 +0900

    123

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -2,6 +2,9 @@
 import myData from './data.js';
 import myData2 from './data2.js';
 
+// JS기능함수
+import { initFn,firstOneFn } from './act_effect.js';
+
 // 두개의 데이터를 배열로 구성
 const twoData = [myData,myData2];
 
@@ -32,7 +35,20 @@ function MainComponent(){
 
     console.log('컴포넌트 그냥규욕',
         document.querySelector('.img-box'));
-        
+
+////////////////////////////////////////////////////////////////////
+// [ 리액트 컴포넌트 랜더링 후 실행함수 호출하기]//////////////////////
+////////////////////////////////////////////////////////////////////       
+    // [ 컴포넌트가 뿌려지기 애니메이션 적용하기 ]
+    React.useLayoutEffect(initFn);
+
+    // 처음 한번만 타이틀 글자 커졌다가 작아지기
+    React.useEffect(firstOneFn,[]);
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+        //[ useEffect 테스트 코드 ] ////////////////////
         // -> 페이지 로딩후 >> 매법 업데이트 시에도 실행함!!
         React.useEffect(()=>{
             console.log('useEffect순수');
@@ -54,8 +70,12 @@ function MainComponent(){
     // 의존성 배열옵션 useEffect
     // -> 페이지 로딩후 >> 단 한번만 실행함 
     React.useEffect(()=>{
-        console.log('useEffect 의존성 배열옵션');             
+        console.log('useEffect 의존성 배열옵션 test');             
     },[test]); ////////////////////////////////////////////
+
+    React.useEffect(()=>{
+        console.log('useEffect 의존성 배열옵션 dataNum');             
+    },[dataNum]); ////////////////////////////////////////////
     // 의존성이 다수일 경우 배열형태의 옵션에
     // 콤마로 연결하여 등록해준다!
             
@@ -65,7 +85,11 @@ function MainComponent(){
     });
 
 
-
+    // 의존성 테스트 함수 ////
+    const testFn = () => {
+        setTest(test?0:1);
+        console.log('test 후크변수 변경',test);
+    }; //// testFn //////////
 
     // 데이터 변경 호출 함수 /////////////////
     const chgData = () => {
@@ -95,6 +119,7 @@ function MainComponent(){
             <button onClick = {chgData} className = "btn-gong">
                 {dataNum?"공유초이스 바로가기":"효진초이스 바로가기"}
             </button>
+            <button onClick={testFn}>의존성테스트</button>
             {/* 4. 상품리스트박스 */}
             <div className="gwrap">
                 <GoodsCode idx={dataNum} />
@@ -129,6 +154,7 @@ function GoodsCode(props){ // idx - 데이터 배열순번
         ));
 
 }; /////////// GoodsCode //////////////////
+
 
 
 // 메인 컴포넌트 출력하기 ////////////////////////
---
+++ 004.React학습/react-ex/js/act_effect.js
@@ -0,0 +1,26 @@
+// JS 함수 : 제이쿼리 코드를 실행하는 함수 ////
+function initFn(){
+    // 메인 이미지박스 투명하게 
+        $('.img-box').css({opacity:0})
+        .delay(1000).fadeTo(500,1)
+        // .fadeTo(시간,오파,이징,함수) -> 투명도만 애니!
+    
+        // 상품리스트박스 숨기기
+        $('.gwrap').hide().delay(1500).slideDown(2000);
+    }////// initFn /////////////////////////////////////
+    
+    // 처음에 한번만 실행하는 기능 JS함수 ///////////////////
+    function firstOneFn(){
+        $('.tit').css({
+            transform:"scale(2)",
+            transition:"1s ease-out 1s"
+        })
+        setTimeout(() => {
+            $('.tit').css({
+                transform:"scale(1)",
+                transition:"1s ease-out 1s"
+            })
+        }, 2500);
+    }/// firstOneFn ///////////////////////////
+
+    export {initFn,firstOneFn};
