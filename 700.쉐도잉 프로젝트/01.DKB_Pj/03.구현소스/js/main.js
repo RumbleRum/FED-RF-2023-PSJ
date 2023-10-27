ab2d578509ccf2cbc6ced3c6bf17170718e2708d
RumbleRum <dnjswls0339@naver.com>
2023-10-27 18:19:32 +0900

    1232313

--- 700.쉐도잉 프로젝트/01.DKB_Pj/03.구현소스/js/main.js
+++ 700.쉐도잉 프로젝트/01.DKB_Pj/03.구현소스/js/main.js
@@ -6,64 +6,74 @@ import dFn from "./dom.js";
 // 부드러운 스크롤 모듈
 import { startSS, setPos } from "./smoothScroll23.js";
 // 데이터 모듈
-import { gridData, gnbData, previewData, clipData , linkData} from "./data_drama.js";
+import { gridData, gnbData, previewData, clipData, linkData } from "./data_drama.js";
 
 // 부드러운 스크롤 적용 //////////
 startSS();
 
-
-// 모바일 적용여부 코드 //////
+///////////////////////////////
+// 모바일적용 여부 코드 ////////
 let mob = 0; // 0-DT, 1-모바일
-// 모바일 검사 함수 /////////
-const chkMob = () => {if($(window).width()<=1024)mob=1;
+// 모바일 검사함수 /////
+const chkMob = () => {
+if($(window).width()<=1024)mob=1;
 else mob=0;
 console.log('모바일?',mob);
 
-// 부가기능 : 모바일일때 기본 스타일 지우기
+// 부가기능 : 모바일일때 서브메뉴 기본 스타일 지우기
 if(mob) $('.smenu').attr('style','');
 
-} ////// chkMob함수 ///////////////////
+} /////// chkMob함수 ///////
 
 // 모바일 검사함수 최초호출
 chkMob();
 // 화면 리사이즈 시 모바일 검사함수 호출
 $(window).resize(chkMob);
-////////////////////////////////////////////////////////////
+//////////////////////////////////////
 
-///////////////////////////////////////////////////////////
-// 모바일시 기능구현 /////////////////////////////////////////
-////////////////////////////////////////////////////////////
-
-// 1. 햄버거 버튼 클릭시 메뉴 보이기 / 숨기기
-// 대상 : .ham / .header
+///////////////////////////////////
+// 모바일시 기능구현 ///////////////
+///////////////////////////////////
+// 1. 햄버거 버튼 클릭시 메뉴 보이기/숨기기
+// 대상: .ham / .header
 const hEle = $('.header');
 $('.ham').click(()=>{
   hEle.toggleClass('on');
 
-  console.log('지금.header에 .on있나',$('.header').is('.on'));
+  // is() 메서드 : 선택요소의 이름확인!
+  console.log('지금.header에 .on있니?',hEle.is('.on'));
   // 만약 .header.on 이면 body에 스크롤바 숨기기
-  if(hEle.is(".on")) 
+  if(hEle.is('.on')) 
     $('html,body').css({overflow:'hidden'});
   // 아니면 넣었던 스타일 지우기
   else $('html,body').attr('style','');
 
-}); //////// click /////////////////////
+}); //////// click ////////////
 
 
-// 2. 메뉴 클릭시 하위메뉴 보이기
-// 대상 : .gnb>li
+// 2. 메뉴 클릭시 하위메뉴 보이기 //////
+// 대상: .gnb>li
 $('.gnb li').click(function(){
-  if(!mob) return; // 모바일이 아니면 나가!
-  console.log('날클릭');
+  if(!mob) return;//모바일 아니면 나가!
+  console.log('나클릭?');
   // 서브메뉴 슬라이드 애니로 보이기/숨기기
-  // 대상 : .smenu
-  $(this).find('.smenu')
-  .slideToggle(700,'easeInOutQuad') // 열거나 닫기
+  // 대상: .smenu
+  $(this).find('.smenu') // 클릭된li 하위 .smenu
+  .slideToggle(300,'easeInOutQuad') // 열거나 닫거나함
   .parent() // 부모로 올라감 li
-  .siblings().find('.smenu') // 다른 li들 하위
-  .slideUp(700,'easeInOutQuad') // 스르륵 닫힘 모두
+  .siblings().find('.smenu') // 다른 li들 하위 .smenu
+  .slideUp(300,'easeInOutQuad') // 스르륵 닫힘! 모두
 
-}); ///////click //////////////////////////
+}); //////////// click ////////////////
+
+// 3. 스티키 메뉴 박스 드래그 하여 움직여보기
+// 대상: .dokebi-menu ul
+$('.dokebi-menu ul')
+.draggable({
+  axis:'x', // x축고정
+}); /////////// draggable ////////////
+
+//////////////////////////////////////////////
 
 
 
@@ -210,7 +220,7 @@ gnb.forEach((ele) => {
 // 3.함수만들기
 function overFn() {
   if(mob)return; // 모바일이면 나감!
-  console.log('오버',this);
+  // console.log('오버',this);
   // 1.하위 .smbx 높이값 알아오기
   let hv = dFn.qsEl(this, ".smbx").clientHeight;
   // // console.log("높이:", hv);
@@ -219,7 +229,7 @@ function overFn() {
 } //////////// overFn 함수 ////////////
 
 function outFn() {
-  if(mob) return; // 모바일이면 나감!
+  if(mob)return; // 모바일이면 나감!
   // // // console.log('아웃',this);
   // 서브메뉴 박스 높이값 0만들기!
   dFn.qsEl(this, ".smenu").style.height = "0px";
@@ -349,11 +359,9 @@ btnClip.forEach((ele) => {
 
 // 5. 함수 만들기 ////////////////
 function moveClip() {
-
   // 1. 오른쪽 버튼 여부
   let isR = this.classList.contains("fa-chevron-right");
   // console.log("나야나!", isR);
-
   // 2. 버튼별 이동분기
   if (isR) {
     // 오른쪽버튼
@@ -371,7 +379,6 @@ function moveClip() {
       btnClip[0].style.display = 'block';
     }
   } //////// if ////////
-
   else {
     // 왼쪽버튼
     // 이동한계수를 체크하여 이동수를 감소시킴
@@ -387,101 +394,102 @@ function moveClip() {
       // 마지막버튼 보이기
       btnClip[1].style.display = 'block';
     }
-  } //////// else ////////
+  } //////// if ////////
 
   // 3. 이동반영하기 : - (단위수*이동수) %
   clipList.style.left = -(BLOCK_NUM * mvNum) + "%";
 } /////////// moveClip 함수 //////////
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-// 하단링크 콤보 박스 바인딩하기 /////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////
+// 하단링크 콤보 박스 바인딩하기 ////////////
+//////////////////////////////////////////
 // 1. 요구사항 - 콤보박스에 맞는 데이터를 바인딩한다
 // 2. 데이터 - linkData
-// // console.log('콤보박스',linkData);
+// // console.log('하단콤보박스 데이터:',linkData);
 
 // 3. 대상선정 : 바인딩할 콤보박스
 // #brand, #corp
 const brandBox = dFn.qs('#brand');
 const corpBox = dFn.qs('#corp');
-// console.log('콤보박스',brandBox,corpBox);
+// console.log('콤보박스:',brandBox,corpBox);
 
 // 4. 데이터 바인딩하기
 // 4-1. 브랜드 바로가기 콤보박스 : 단순바인딩(option만)
 // 데이터 대상: linkData.brand
-brandBox.innerHTML = '';  /* 원래 브랜드 바로가기 내용은 날리기 */
+
+// 내부초기화
+brandBox.innerHTML = '';
 
 linkData.brand.forEach(val=>{
-  brandBox.innerHTML += `
-    <option value="${val}">${val}</option>
-  `;
-}); /////forEach/////////////////
+  brandBox.innerHTML += 
+  `<option value="${val}">${val}</option>`;
+}); ///////// forEach ////////
 
-// 4-2. 계열사 바로가기 콤보박스 : 복합바인딩(optgroup>option)
-// 데이터를 객체형이므로 속성만 모아 배열로 변환하여 
+// 4-2. 계열사 바로가기 콤보박스 : 
+// -> 복합바인딩(optgroup>option)
+// 데이터는 객체형이므로 속성만 모아 배열로 변환하여
 // forEach를 사용한다!
-const corpData = Object.keys(linkData.corp); 
+const corpData = Object.keys(linkData.corp);
 
 // 내부 초기화
 corpBox.innerHTML = '';
 
-
-
-// // console.log(corpData);
+// // console.log('계열사 데이터:',corpData);
 corpData.forEach(val=>{
   corpBox.innerHTML += `
   <optgroup label="${val}">
-        ${linkData.corp[val].map(v=>`<option value="${v}">${v}</option>`).join('')}
-   </optgroup>
+    ${linkData.corp[val].map(v=>
+      `<option value="${v}">${v}</option>`).join('')}
+  </optgroup>
   `;
-}); //////// forEach ////////////
+}); //////////// forEach /////////////
 
-
-// 내부의 option 데이터 요소는 배열데이터.map().join('')을 사용!
-// 맵쬬인
-// map() 메서드 리마인딩
+// 내부의 option요소는 배열데이터.map().join('')을 사용!
+// 맵쬬잉~!!!
+// map() 메서드 리마인딩!
 // map()은 배열을 재구성하여 다시 같은 자리에 리턴하여
-// 만들어지는 새로운 배열을 변수에 담거나 그자리에 리턴한다
+// 만들어진 새로운 배열을 변수에 담거나 그자리에 리턴한다
 // 이때 배열값을 문자열 값으로 변환하는 join()을 사용하여
 // 연결자를 빈값으로 처리하면 배열의 구분자 콤마가 없는
-// 태그로만 연결된 순수한 태그 결과 문자열이 만들어진다
+// 태그로만 연결된 순수한 태그 결과 문자열이 만들어진다!
 
 
-/* 
-
-  [복합바인딩 요소 구성형식]
+/***************************************** 
+  [ 복합바인딩 요소 구성형식 ]
   <optgroup label="Swedish Cars">
     <option value="volvo">Volvo</option>
     <option value="saab">Saab</option>
   </optgroup>
+*****************************************/
 
-*/
+/////////////////////////////////////////////
 
-///////////////////////////////////////////////////
+/// 제이쿼리로 기능구현하기 //////////////////
 
-// 제이쿼리로 기능구현하기 //////////////////////
+// 1. 서브컨텐츠 보이기 기능구현 /////////
 
-// 1. 서브컨텐츠 보이기 기능구현 //////////////////
-// (1) 대상 : 
-// 이벤트 대상 : .sub-view-box 하위 .partbox 또는 li 
-const subViewBox = $('.sub-view-box .partbox, .sub-view-box li');
+// (1) 대상선정 : 
+// 이벤트 대상 : .sub-view-box 하위 .partbox 또는 li
+const subViewBox = 
+$('.sub-view-box .partbox,.sub-view-box li');
 // 변경대상 : .sub-cont
 const subContBox = $('.sub-cont')
-// console.log(subViewBox);
+// console.log(subContBox);
 
-// (2) 이벤트 함수 만들기 ////////////////
+// (2) 이벤트 함수 만들기 ////////
 subViewBox.click(function(){
-  console.log('ㅇㅇ',this);
+  console.log('나야나!',this);
 
-  // 제목 읽어오기
-  let subTit = $(this).parents('.sub-view-box').prev().text();
-  // 나자신.부모들(특정클래스).이전형제().글자();
+  // 1. 제목 읽어오기
+  let subTit = 
+  $(this).parents('.sub-view-box').prev().text();
+  // 나자신.부모들(특정클래스).이전형제().글자읽기();
 
-  // 내용 읽어오기
+  // 2. 내용 읽어오기
   let subItem = $(this).text();
 
-  //1. 서브박스 내용넣기
+  // 3. 서브박스 내용 넣기
   subContBox.html(`
     <button class="cbtn">×</button>
     <div class="sub-inbox inbox">
@@ -492,9 +500,10 @@ subViewBox.click(function(){
     </div>
   `);
 
-  // 2. 닫기버튼 이벤트 설정
+  // 3. 닫기버튼 이벤트설정
   $('.cbtn').click(()=>subContBox.hide())
+
 
   // 999. 서브박스 보이기
   subContBox.show();
-}); ////click///////
+}); /////////// click //////////
