5d64c0d2a8eb14b7d72c0360d247344f3a17adf1
RumbleRum <dnjswls0339@naver.com>
2023-11-08 14:49:51 +0900

    ss

---
+++ 004.React학습/react-app/psj-app/my-first-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          절대 리엑트를 검색하지마!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
