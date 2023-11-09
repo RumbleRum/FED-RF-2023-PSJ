69258ccb1263056292d98a0252338ae2c670b237
RumbleRum <dnjswls0339@naver.com>
2023-11-09 15:19:36 +0900

    213213

--- 004.React학습/react-app/psj-app/my-first-react-app/src/index.js
+++ 004.React학습/react-app/psj-app/my-first-react-app/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+// index.js는 public/index.html 페이지에 적용되는 컴포넌트다!->루트 컴포넌트
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// css 도 불러온다!
+import './css/index.css';
+import { TopArea } from './dc/TopArea';
+import { MainArea } from './dc/MainArea';
+import { FooterArea } from './dc/FooterArea';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App(){
+  return(
+    <>
+    <TopArea />
+    <MainArea />
+    <FooterArea />
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// 컴포넌트 출력 //////////
+// 먼저 root객체만들고
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+// render메서드로 출력
+root.render(<App />);
