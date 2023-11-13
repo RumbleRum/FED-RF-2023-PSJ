813dfed4d55e2e2205da8b9ad04da1ce66924d7a
RumbleRum <dnjswls0339@naver.com>
2023-11-14 02:33:07 +0900

    123213

---
+++ 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/FooterArea.jsx
@@ -0,0 +1,12 @@
+// FooterArea영역 공통 컴포넌트
+
+export function FooterArea(){
+
+        return(
+            <>
+            
+                <h1>하단영역</h1>
+            </>
+        );
+
+} /// FooterArea //////////////////////////////
