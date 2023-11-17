b1c364a3a929212121b3748c865a8861c5b9ae77
RumbleRum <dnjswls0339@naver.com>
2023-11-17 11:10:58 +0900

    4214214

--- 004.React학습/react-app/psj-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/psj-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -1,6 +1,5 @@
 // 스와이퍼 플러그인 컴포넌트
 
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,8 +11,9 @@ import 'swiper/css/navigation';
 import "./css/swiper.css";
 
 // import required modules
-// 사용할 스와이퍼 모듈을 불러온다(여기서는 페이지네이션, 네비게이션 , 자동넘김 사용)
-import { Pagination,Navigation,Autoplay } from "swiper/modules";
+// 사용할 스와이퍼 모듈을 불러온다
+// (여기서는 페이지네이션,네비게이션,자동넘김)
+import { Pagination,Navigation,Autoplay } from "swiper/modules"
 
 export function SwiperApp() {
   // 불러올 이미지 리스트
@@ -27,6 +27,7 @@ export function SwiperApp() {
     "dcm11",
     "dcm12",
   ];
+
 
   return (
     <>
@@ -42,7 +43,7 @@ export function SwiperApp() {
         }}      
         loop={true}
         navigation={true}
-        // 사용할 모듈을 여기에 적용시킨다 ///////
+        /* 사용할 모듈을 여기에 적용시킨다 */
         modules={[Pagination,Navigation,Autoplay]}
         className="mySwiper"
       >
@@ -52,7 +53,6 @@ export function SwiperApp() {
                 <img src={"./images/"+v+".jpg"} alt="list image" />
             </SwiperSlide>)
         }        
-       
       </Swiper>
     </>
   );
