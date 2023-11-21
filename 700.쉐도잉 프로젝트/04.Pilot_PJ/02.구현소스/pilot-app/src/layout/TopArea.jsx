d04934093a9fe9f279dcb4ae234e8294dc353e11
RumbleRum <dnjswls0339@naver.com>
2023-11-21 12:00:22 +0900

    4214

--- 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/TopArea.jsx
+++ 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/TopArea.jsx
@@ -2,96 +2,47 @@
 
 // GNB 데이터 가져오기
 import { gnbData } from "../data/gnb";
+import { TotalMenu } from "../modules/TotalMenu";
 
 export function TopArea(props) {
-  // props.cat  - 카테고리명
+  // props.cat - 카테고리명(메뉴데이터 선택용)
 
-    // GNB메뉴 리스트 만들기 함수
-    const makeList = (data) => {
-        return(
-        gnbData[data].map((v, i) => (
-            <li key={i}>
-                <a href="#">{v}</a>
-            </li>
-        )));
-    }; /// makeList ////////////////////////
+  /// GNB메뉴 리스트 만들기 함수
+  const makeList = (data) => {
+    return(
+      gnbData[data].map((v,i)=>
+        <li key={i}>
+          <a href="#">{v}</a>
+        </li>
+      )
+    )
 
-    return (
-        <>
-            <div id="top-area">
-                <header className="top-area ibx">
-                    <h1 id="logo">
-                        <a href="#">
-                            <img src="./images/main_logo.png" alt="파일럿로고" />
-                        </a>
-                    </h1>
-                    <nav className="gnb">
-                        <ul>
-                            <li className="bld">배너순번 li 숨기기</li>
-                            {makeList(props.cat)}
-                        </ul>
-                    </nav>
-                    <div className="ham">
-                        <span></span> <span></span> <span></span>
-                    </div>
-                    <div className="mbox">
-                        <video src="images/disc2018.mp4" loop="loop" muted="muted" className="bgm"></video>
-                        <nav className="mlist">
-                            <dl>
-                                <dt>
-                                    <a href="sub.html?cat=남성">MEN</a>
-                                </dt>
-                                <dd>
-                                    <a href="#">T-SHIRT</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">JACKET</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">TRAINING WARE</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">BEACH WARE</a>
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt>
-                                    <a href="sub.html?cat=여성">WOMEN</a>
-                                </dt>
-                                <dd>
-                                    <a href="#">T-SHIRT</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">JACKET</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">TRAINING WARE</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">BEACH WARE</a>
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt>
-                                    <a href="sub.html?cat=스타일">STYLE</a>
-                                </dt>
-                                <dd>
-                                    <a href="#">COLLECTION</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">SEASON AD</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">STAR &amp; NEWS</a>
-                                </dd>
-                                <dd>
-                                    <a href="#">MAIN ITEM</a>
-                                </dd>
-                            </dl>
-                        </nav>
-                    </div>
-                </header>
-            </div>
-        </>
-    );
+  }; ///////// makeList /////////
+
+
+
+  return (
+    <>
+      <div id="top-area">
+        <header className="top-area ibx">
+          <h1 id="logo">
+            <a href="#">
+              <img src="./images/main_logo.png" alt="파일럿로고" />
+            </a>
+          </h1>
+          <nav className="gnb">
+            <ul>
+              <li className="bld">배너순번 li 숨기기</li>
+              {makeList(props.cat)}
+            </ul>
+          </nav>
+          <div className="ham">
+            <span></span> <span></span> <span></span>
+          </div>
+          {/* 전체메뉴 컴포넌트 */}
+          <TotalMenu />
+        </header>
+      </div>
+    </>
+  );
 } //////////////// TopArea 컴포넌트 //////////
