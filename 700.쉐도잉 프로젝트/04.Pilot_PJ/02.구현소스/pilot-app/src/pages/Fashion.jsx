908c72a60b5bfdd738b5fe5250269e18e7e9e397
RumbleRum <dnjswls0339@naver.com>
2023-11-28 09:56:24 +0900

    32142141241

--- 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -6,43 +6,46 @@ import { useContext, useEffect } from "react"
 import "../css/fashion.css";
 import { SwiperApp } from "../plugin/SwiperApp";
 
-// 켄텍스트 API
-import { pCon } from '../modules/PilotContext';
+// 컨텍스트 API
+import { pCon } from "../modules/PilotContext";
 
 // 제이쿼리
 import $ from 'jquery';
+import { SinSang } from "../modules/SinSang";
 
 export function Fashion(props){
-    // props.cat - 서브 카테고리명
-
+    // 컨텍스트 API 사용!
     const myCon = useContext(pCon);
+
+    // props.cat - 서브 카테고리명
 
     useEffect(()=>{
         // 스크롤바 생성하기
         $('html,body').css({overflow:'visible'});
 
-        // 로고 클릭시 페이지 이동 : 
+        // 로고클릭시 페이지이동 : pgName 변경 -> chgPgName()
         $("#logo a").click(()=>myCon.chgPgName('main'));
 
-    },[]); //////////// useEffect /////////////////
-
+    },[])
 
     return(
         <>
             {/* 1. 배너영역 */}
-           <section id="ban" className="page">
-            <SwiperApp cat={myCon.pgName}/>
-           </section>
-           {/* 2. 신상품영역 */}
-           <section id="c1" className="cont c1 men"></section>
-           {/* 2.5. 상세보기박스 */}
-           <div className="bgbx"></div>
-           {/* 3. 패럴렉스 영역 */}
-           <section id="c2" className="cont c2 men"></section>
-           {/* 4. 단일상품 영역 */}
-           <section id="c3" className="cont c3"></section>
-           {/* 5. 스타일 상품영역 */}
-           <section id="c4" className="cont c4"></section>
+            <section id="ban" className="page">
+                <SwiperApp cat={myCon.pgName} />
+            </section>
+            {/* 2. 신상품영역 */}
+            <section id="c1" className={"cont c1 "+myCon.pgName}>
+                <SinSang cat={myCon.pgName} />
+            </section>
+            {/* 2.5. 상세보기박스 */}
+            <div className="bgbx"></div>
+            {/* 3. 패럴랙스 영역 */}
+            <section id="c2" className="cont c2 men"></section>
+            {/* 4. 단일상품영역 */}
+            <section id="c3" className="cont c3"></section>
+            {/* 5. 스타일상품영역 */}
+            <section id="c4" className="cont c4"></section>
         </>
     )
 
