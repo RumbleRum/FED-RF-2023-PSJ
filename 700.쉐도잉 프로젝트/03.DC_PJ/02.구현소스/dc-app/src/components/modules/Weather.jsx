c03544776c7635f92343e2288f1818e5e34b2ef6
RumbleRum <dnjswls0339@naver.com>
2023-12-27 16:20:32 +0900

    asdd

--- 700.쉐도잉 프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Weather.jsx
+++ 700.쉐도잉 프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Weather.jsx
@@ -1,5 +1,11 @@
 import { Component } from "react";
 
+// 날씨 CSS
+import "../../css/weather.css";
+
+// 엑시오스 불러오기
+import axios from "axios";
+
 // 여기서는 컴포넌트를 class로 만들어 보자!
 // 컴포넌트 class는 기본적으로 Component 클래스를 상속받는다!
 class Weather extends Component {
@@ -14,7 +20,7 @@ class Weather extends Component {
         // 클래스 내부속성은 this키워드를 사용함!
         // 받아온 날씨정보를 셋업할 객체임!
         // state 이름의 상태변수에 setState()로 셋팅함
-        this.state = { temp: "", desc: "", icon: "", loading: true };
+        this.state = { temp: "", desc: "", icon: "", loading: true, city: "" };
     } ///// 생성자함수 /////////////
 
     // 컴포넌트 생성후 날씨정보 조회하여 화면에 보이기
@@ -25,7 +31,7 @@ class Weather extends Component {
         // https://openweathermap.org/
 
         // 1. 지정도시명
-        const cityName = "Seoul";
+        const cityName = "New York";
         // 2. 날씨정보조회 키코드(로그인사용자 키복사)
         const apiKey = "7fdf8fb74f3e2ed02bfb7e298a32df49";
         // 3. 날씨정보조회 url : 날씨정보 제이슨이 날아온다!
@@ -37,28 +43,60 @@ class Weather extends Component {
         // 소스샘플
         // https://openweathermap.org/api/one-call-3
 
-        // fetch() 함수를 이용한 데이터 조회하기!
-        fetch(url) // 파일받기
-            .then((res) => res.json()) // json() 제이슨파일형식파싱
-            .then((wdata) => {
-                //파일파싱후후 내용읽기
-                console.log(wdata, wdata.main.temp);
-                // 상태변수인 wInfo에 값을 셋팅한다!
-                // 셋팅용 상태변수 메서드형은 setState()
-                // this키워드 사용!
+        //////////////////////////////////////////////////
+        // axios 라이브러리를 이용한 데이터 조회하기 ! //////
+        // 먼저 설치 : npm i axios
+        // 엑시오스는 데이터를 promise 로 처리하여 원하는 결과를 보장하는 간편한 데이터처리 라이브러리다
+
+        // 엑시오스 사용법
+        // 우선상단에 import axios 해줌
+        // 파일 가져오기 메서드 : get()
+        // 다음 실행 메서드 : then()
+        axios
+            .get(url) // 파일로딩
+            .then((res) => {
+                // 파일형식에 맞는 파싱 자동!
+                console.log(res);
+                // 주의: 로그에서 확인한 바와 같이
+                // data 속성에 실제 데이터가 담긴다!
+                // 들어오는 변수인 res.data 해야함
+                const wdata = res.data;
+
+                // 상태변수값에 셋팅하기 /////////
                 this.setState({
                     temp: wdata.main.temp,
                     desc: wdata.weather[0].description,
                     icon: wdata.weather[0].icon,
                     loading: false, // 로딩여부끝(false)
+                    city: cityName,
                 });
-            }) /////// 마지막 then /////
-            // 에러시 처리
-            .catch((err) => console.log(err));
+        
+            }) //// then //////
+            .catch(err=>console.log(err));
+            ////// axios 끝 //////////////////////
+
+        // fetch() 함수를 이용한 데이터 조회하기!
+        // fetch(url) // 파일받기
+        //     .then((res) => res.json()) // json() 제이슨파일형식파싱
+        //     .then((wdata) => {
+        //         //파일파싱후후 내용읽기
+        //         console.log(wdata, wdata.main.temp);
+        //         // 상태변수인 wInfo에 값을 셋팅한다!
+        //         // 셋팅용 상태변수 메서드형은 setState()
+        //         // this키워드 사용!
+        //         this.setState({
+        //             temp: wdata.main.temp,
+        //             desc: wdata.weather[0].description,
+        //             icon: wdata.weather[0].icon,
+        //             loading: false, // 로딩여부끝(false)
+        //             city: cityName,
+        //         });
+        //     }) /////// 마지막 then /////
+        //     // 에러시 처리
+        //     .catch((err) => console.log(err));
     } ////////// componentDidMount 메서드 ///////////
 
     // 컴포넌트 결과 랜더링 메서드 //////
-    // render()
     render() {
         // 아이콘 정보
         const isrc = `http://openweathermap.com/img/w/${this.state.icon}.png`;
@@ -69,11 +107,17 @@ class Weather extends Component {
         } // if //////
         // 로딩성공시 loading 값이 false이면
         else {
-            return(
+            {
+                /* 절대온도이므로 섭씨온도로 바꾼다! 소수점도 한자리만 표시 */
+            }
+            let ctemp = (parseInt(this.state.temp) - 273.15).toFixed(1);
+
+            return (
                 <div className="weather-bx">
                     <h4>Now Weather</h4>
+                    <h5>{this.state.city}</h5>
                     <img src={isrc} alt="weather icon" />
-                    <p>{this.state.temp}</p>
+                    <p>{ctemp}℃</p>
                     <p>{this.state.desc}</p>
                 </div>
             );
