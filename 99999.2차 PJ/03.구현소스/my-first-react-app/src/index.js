ff10d3348169e99e9320b984ab8af0b4c324bb82
RumbleRum <dnjswls0339@naver.com>
2023-12-28 18:17:45 +0900

    dsadasdsad

--- 99999.2차 PJ/03.구현소스/my-first-react-app/src/index.js
+++ 99999.2차 PJ/03.구현소스/my-first-react-app/src/index.js
@@ -10,35 +10,39 @@ import ReactDOM from "react-dom/client";
 import "./css/main.css";
 import { MainPage } from "./components/pages/MainPage";
 import { SubPage } from "./components/pages/SubPage";
+import { Gaip } from "./components/pages/Gaip";
+import { Login } from "./components/pages/Login";
 import { SubPage_HT } from "./components/pages/SubPage_HT";
 
+
 function App() {
-  const [pgName, setPgName] = useState("main");
+    const [pgName, setPgName] = useState("main");
 
-  const chgPage = (pg) => {
-    setPgName(pg);
-    console.log(pgName);
-  };
+    const chgPage = (pg) => {
+        setPgName(pg);
+        console.log(pgName);
+    };
 
-
-  // 리턴 코드 ////////////////////
-  return (
-    <>
-      <div className="wrap">
-        {pgName === "main" ? (
-          <MainPage chgPg={chgPage} />
-        ) : pgName === "sub" ? (
-          <SubPage chgPg={chgPage} />
-        ) : pgName === "sub2" ? (
-          <SubPage_HT chgPg={chgPage} />
-        ) 
-          :
-        (
-          <MainPage chgPg={chgPage} />
-        )}
-      </div>
-    </>
-  );
+    // 리턴 코드 ////////////////////
+    return (
+        <>
+            <div className="wrap">
+                {pgName === "main" ? (
+                    <MainPage chgPg={chgPage} />
+                ) : pgName === "sub" ? (
+                    <SubPage chgPg={chgPage} />
+                ) : pgName === "sub2" ? (
+                    <SubPage_HT chgPg={chgPage} />
+                ) : pgName === "gaip" ? (
+                    <Gaip chgPg={chgPage} />
+                ) : pgName === "login" ? (
+                    <Login chgPg={chgPage} />
+                ) : (
+                    <MainPage chgPg={chgPage} />
+                )}
+            </div>
+        </>
+    );
 }
 
 // 컴포넌트 출력 //////////
