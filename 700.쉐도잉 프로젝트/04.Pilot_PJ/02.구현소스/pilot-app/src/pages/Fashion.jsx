1cec1fc383aac172b171952e44a9b8171b51ad78
RumbleRum <dnjswls0339@naver.com>
2024-01-15 11:14:55 +0900

    asdasd

--- 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉 프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -135,16 +135,16 @@ export function Fashion(props) {
           // 수치범위 :  -500 ~ 1000 -> 높은 숫자는 반대방향
           strength={200}
         >
-          <h2 class="c2tit">2024 {gnbData[props.cat][1]}</h2>
+          <h2 className="c2tit">2024 {gnbData[props.cat][1]}</h2>
         </Parallax>
       </section>
       {/* 4. 단일상품영역 */}
       <section id="c3" className="cont c3">
-        <FashionIntro cat="women" />
+        <FashionIntro cat="sub" subcat={[props.cat,0]} />
       </section>
       {/* 5. 스타일상품영역 */}
       <section id="c4" className="cont c4">
-        <FashionIntro cat="style" />
+        <FashionIntro cat="sub" subcat={[props.cat,1]} />
       </section>
     </>
   );
