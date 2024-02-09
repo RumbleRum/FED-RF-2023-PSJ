87482378f6bce5ad4e49d8c65718c93e8b0aa91b
RumbleRum <dnjswls0339@naver.com>
2024-02-09 17:40:33 +0900

    dasdasd

--- 99999.포폴용/js/main.js
+++ 99999.포폴용/js/main.js
@@ -1,1 +1,14 @@
 /* 메인 JS */
+
+const tabContain = document.querySelector(".about-tab"),
+    aboutSection = document.querySelector(".about-section");
+
+tabContain.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+        tabContain.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
